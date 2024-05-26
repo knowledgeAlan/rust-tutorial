a253bde3ba2c8b1867c7bd925f26e83b2bdcafcc
zhongzuomin <1299076979@qq.com>
2024-05-26 20:59:40 +0800

    tutorial use io

---
+++ rust-tutorial/tutorial-four/target/debug/deps/tutorial_four-dabbafae9b4dbee7.d
@@ -0,0 +1,5 @@
+/Users/zhongzuoming/Desktop/rust-workspace/rust-tutorial/tutorial-four/target/debug/deps/tutorial_four-dabbafae9b4dbee7: src/main.rs
+
+/Users/zhongzuoming/Desktop/rust-workspace/rust-tutorial/tutorial-four/target/debug/deps/tutorial_four-dabbafae9b4dbee7.d: src/main.rs
+
+src/main.rs:
---
+++ rust-tutorial/tutorial-four/target/debug/tutorial-four.d
@@ -0,0 +1,1 @@
+/Users/zhongzuoming/Desktop/rust-workspace/rust-tutorial/tutorial-four/target/debug/tutorial-four: /Users/zhongzuoming/Desktop/rust-workspace/rust-tutorial/tutorial-four/src/main.rs
