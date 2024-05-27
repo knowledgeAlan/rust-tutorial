231befa78fe5dbb928cd6da64aa785ec902334e7
zzm <zzm12990@gmail.com>
2024-05-27 15:10:50 +0800

    init

---
+++ rust-tutorial/tutorial1/target/debug/deps/tutorial1-f91f2fced5571116.d
@@ -0,0 +1,5 @@
+/Users/uniweb/Desktop/rust-workspace/rust-tutorial/rust-tutorial/tutorial1/target/debug/deps/libtutorial1-f91f2fced5571116.rmeta: src/main.rs
+
+/Users/uniweb/Desktop/rust-workspace/rust-tutorial/rust-tutorial/tutorial1/target/debug/deps/tutorial1-f91f2fced5571116.d: src/main.rs
+
+src/main.rs:
