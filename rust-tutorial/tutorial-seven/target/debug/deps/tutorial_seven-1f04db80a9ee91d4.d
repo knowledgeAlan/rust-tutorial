4df1167120721438b7448ee3d07562efb7a45038
zzm <zzm12990@gmail.com>
2024-06-11 18:37:15 +0800

    add package

---
+++ rust-tutorial/tutorial-seven/target/debug/deps/tutorial_seven-1f04db80a9ee91d4.d
@@ -0,0 +1,5 @@
+/Users/uniweb/Desktop/rust-workspace/rust-tutorial/rust-tutorial/tutorial-seven/target/debug/deps/libtutorial_seven-1f04db80a9ee91d4.rmeta: src/main.rs
+
+/Users/uniweb/Desktop/rust-workspace/rust-tutorial/rust-tutorial/tutorial-seven/target/debug/deps/tutorial_seven-1f04db80a9ee91d4.d: src/main.rs
+
+src/main.rs:
---
+++ rust-tutorial/tutorial-seven/target/debug/deps/tutorial_seven-7cd0b1e1bb0ff984.d
@@ -0,0 +1,5 @@
+/Users/uniweb/Desktop/rust-workspace/rust-tutorial/rust-tutorial/tutorial-seven/target/debug/deps/libtutorial_seven-7cd0b1e1bb0ff984.rmeta: src/main.rs
+
+/Users/uniweb/Desktop/rust-workspace/rust-tutorial/rust-tutorial/tutorial-seven/target/debug/deps/tutorial_seven-7cd0b1e1bb0ff984.d: src/main.rs
+
+src/main.rs:
